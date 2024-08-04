d8f3a2e97db2ff7801124d5a18759c0866b6a178
Taysen <52516060+T-Victory@users.noreply.github.com>
2024-08-04 15:44:34 -0400

    Improved compatibility across development devices
    Removed node-modules from the gitignore.

--- leadership/app/index.js
+++ leadership/app/index.js
@@ -7,12 +7,25 @@ import Colours from '../hooks/Colours';
 import LoadFonts from '../hooks/LoadFonts';
 import { Link } from 'expo-router';
 
+import Event from '../components/Event';
+import EventView from '../components/EventView';
+
 export default function App() {
   const fontsLoaded = LoadFonts();
 
   if (!fontsLoaded) {
     return null;
   }
+
+
+
+  let name = "Lions Leadership";
+  const date = "10:55 - 12:00";
+  const content = "Coding team only! Bring your forms!";
+
+  const gradient1 = ['#E3F8FF', '#E8FFF8'];
+  const gradient2 = ['#F6EDFF', '#EBEDFF'];
+
 
   return (
     <View style={styles.container}>
@@ -23,6 +36,10 @@ export default function App() {
         <Text style={{ fontFamily: 'Manrope-SemiBold', fontSize: 16 }}>Display</Text>
         <Text style={{ fontFamily: 'Manrope-Light', fontSize: 16, textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur adipiscing elit.</Text>
         <Link href="/calendar">Calendar</Link>
+
+        <EventView name="Hello" time="10:55 - 12:00" content="Coding team only! Bring your forms!" gradient={['#E3F8FF', '#E8FFF8']} />
+        <EventView name="Hello" time="10:55 - 12:00" content="Coding team only! Bring your forms!" gradient={['#E3F8FF', '#E8FFF8']} />
+        <EventView name="Hello" time="10:55 - 12:00" content="Coding team only! Bring your forms!" gradient={['#E3F8FF', '#E8FFF8']} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -45,6 +62,7 @@ const styles = StyleSheet.create({
   body: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: '80%',
+    width: '85%',
+    gap: 8,
   }
 });
